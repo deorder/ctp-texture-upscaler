46c45ca68d281a0e8f4edf254f2d518c65733bd3
TightIz <45295422+TightIz@users.noreply.github.com>
2019-03-13 19:36:46 +0100

    Revert "Added ImageMagick Detection"
    This reverts commit aee9fd826f002f44382db1cdf3aeb7bfc2c233cc.

--- step1_create_tiles.sh
+++ step1_create_tiles.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 shopt -s extglob
 
-THREADS="8"
+THREADS=8
+
+RESIZE=100%
+OVERDRAW=16
+
+TILE_WIDTH=256
+TILE_HEIGHT=256
+
+FILTER=point
+INTERPOLATE=Nearest
+
+INDEX_FILE="./index.txt"
+
 INPUT_DIR="./input"
-
-# Examples
-LR_SCALE=25%
-LR_FILTER=Catrom
-LR_INTERPOLATE=Catrom
-LR_OUTPUT_DIR="./output/LR"
-
-# Ground truth
-HR_SCALE=100%
-HR_FILTER=point
-HR_INTERPOLATE=Nearest
-HR_OUTPUT_DIR="./output/HR"
-
-# Min and max must be equal
-MIN_TILE_WIDTH=128
-MIN_TILE_HEIGHT=128
-
-MAX_TILE_WIDTH=128
-MAX_TILE_HEIGHT=128
-
-# Category regexp for Skyrim SE
-CATEGORY_REGEXP='s/.*_\(a\|b\|d\|e\|g\|h\|m\|n\|p\|s\|an\|bl\|em\|sk\|msn\|rim\)$/\1/ip'
+OUTPUT_DIR="./output_tiles"
 
 for OPTION in "$@"; do
   case ${OPTION} in
@@ -36,75 +27,57 @@ for OPTION in "$@"; do
     INPUT_DIR="${OPTION#*=}"
     shift
     ;;
-    --lr-scale-=*)
-    LR_SCALE="${OPTION#*=}"
+    -o=*|--output-dir=*)
+    OUTPUT_DIR="${OPTION#*=}"
     shift
     ;;
-    --lr-filter=*)
-    LR_FILTER="${OPTION#*=}"
+    -x=*|--index-file=*)
+    INDEX_FILE="${OPTION#*=}"
     shift
     ;;
-    --lr-interpolate-=*)
-    LR_INTERPOLATE="${OPTION#*=}"
+    -r=*|--resize=*)
+    RESIZE="${OPTION#*=}"
     shift
     ;;
-    -l=*|--lr-output-dir=*)
-    LR_OUTPUT_DIR="${OPTION#*=}"
+    -f=*|--filter=*)
+    FILTER="${OPTION#*=}"
     shift
     ;;
-    --hr-scale-=*)
-    HR_SCALE="${OPTION#*=}"
-    shift
-    ;;
-    --hr-filter=*)
-    HR_FILTER="${OPTION#*=}"
-    shift
-    ;;
-    --hr-interpolate-=*)
-    HR_INTERPOLATE="${OPTION#*=}"
-    shift
-    ;;
-    -h=*|--hr-output-dir=*)
-    HR_OUTPUT_DIR="${OPTION#*=}"
+    -l=*|--interpolate=*)
+    INTERPOLATE="${OPTION#*=}"
     shift
     ;;
     -w=*|--tile-width=*)
-    MIN_TILE_WIDTH="${OPTION#*=}"
-    MAX_TILE_WIDTH="${OPTION#*=}"
+    TILE_WIDTH="${OPTION#*=}"
     shift
     ;;
     -h=*|--tile-height=*)
-    MIN_TILE_HEIGHT="${OPTION#*=}"
-    MAX_TILE_HEIGHT="${OPTION#*=}"
+    TILE_HEIGHT="${OPTION#*=}"
+    shift
+    ;;
+    -d=*|--overdraw=*)
+    OVERDRAW="${OPTION#*=}"
     shift
     ;;
     *)
       echo "usage: $@ ..."
       echo "-t, --threads \"<number>\" (default: ${THREADS})"
       echo "-i, --input-dir \"<input dir>\" (default: ${INPUT_DIR})"
-      echo "--lr-scale \"<percentage>\" (default: ${LR_SCALE})"
-      echo "--lr-filter \"<filter>\" (default: ${LR_FILTER})"
-      echo "--lr-interpolate \"<interpolate>\" (default: ${LR_INTERPOLATE})"
-      echo "-l, --lr-output-dir \"<lr output dir>\" (default: ${LR_OUTPUT_DIR})"
-      echo "--hr-scale \"<percentage>\" (default: ${HR_SCALE})"
-      echo "--hr-filter \"<filter>\" (default: ${HR_FILTER})"
-      echo "--hr-interpolate \"<interpolate>\" (default: ${HR_INTERPOLATE})"
-      echo "-h, --hr-output-dir \"<hr output dir>\" (default: ${HR_OUTPUT_DIR})"
-      echo "-w, --tile-width \"<pixels>\" (default: ${MIN_TILE_WIDTH})"
-      echo "-h, --tile-height \"<pixels>\" (default: ${MIN_TILE_HEIGHT})"
+      echo "-o, --output-dir \"<output dir>\" (default: ${OUTPUT_DIR})"
+      echo "-x, --index-file \"<index file>\" (default: ${INDEX_FILE})"
+      echo "-r, --resize \"<percentage>\" (default: ${RESIZE})"
+      echo "-f, --filter \"<filter>\" (default: ${FILTER})"
+      echo "-l, --interpolate \"<interpolate>\" (default: ${INTERPOLATE})"
+      echo "-w, --tile-width \"<pixels>\" (default: ${TILE_WIDTH})"
+      echo "-h, --tile-height \"<pixels>\" (default: ${TILE_HEIGHT})"
+      echo "-d, --overdraw \"<pixels>\" (default: ${OVERDRAW})"
       exit 1
     ;;
   esac
 done
 
-USE_MAGICK=1
-if [ -n 'magick -version | fgrep "ImageMagick 7"' ]
-then
-  echo 'Using magick prefix.'
-else
-  echo 'Using deprecated method.'
-  USE_MAGICK=0
-fi
+:> "${INDEX_FILE}"
+mkdir -p "${OUTPUT_DIR}"
 
 wait_for_jobs() {
   local JOBLIST=($(jobs -p))
@@ -116,73 +89,82 @@ wait_for_jobs() {
   fi
 }
 
+create_tiles_task() {
+
+  INPUT_PATH="$1"
+
+  IMAGE_WIDTH="$3"
+  IMAGE_HEIGHT="$4"
+  IMAGE_CHANNELS="$5"
+
+  OUTPUT_BASENAME="$2"
+
+  ROWS=$((${IMAGE_HEIGHT} / ${TILE_HEIGHT}))
+  if [ "${ROWS}" -le 0 ]; then
+    ROWS=1
+  fi
+  COLUMNS=$((${IMAGE_WIDTH} / ${TILE_WIDTH}))
+  if [ "${COLUMNS}" -le 0 ]; then
+    COLUMNS=1
+  fi
+
+  if [ "${IMAGE_CHANNELS}" == "rgb" ] || [ "${IMAGE_CHANNELS}" == "rgba" ] || [ "${IMAGE_CHANNELS}" == "srgb" ] || [ "${IMAGE_CHANNELS}" == "srgba" ]; then
+    for TILE_COLUMN_INDEX in $(seq 0 $((${COLUMNS} - 1))); do
+      for TILE_ROW_INDEX in $(seq 0 $((${ROWS} - 1))); do
+
+        TILE_INDEX=$(((${TILE_ROW_INDEX} * ${COLUMNS}) + ${TILE_COLUMN_INDEX}))
+        TILE_X1=$((${TILE_COLUMN_INDEX} * ${TILE_WIDTH}))
+        TILE_Y1=$((${TILE_ROW_INDEX} * ${TILE_HEIGHT}))
+        TILE_X2=$(((${TILE_COLUMN_INDEX} * ${TILE_WIDTH}) + ${TILE_WIDTH}))
+        TILE_Y2=$(((${TILE_ROW_INDEX} * ${TILE_HEIGHT}) + ${TILE_HEIGHT}))
+
+        magick convert "${INPUT_PATH}" -alpha off -crop $((${TILE_WIDTH} + ${OVERDRAW}))x$((${TILE_HEIGHT} + ${OVERDRAW}))+${TILE_X1}+${TILE_Y1} +repage +adjoin -define png:color-type=2 -interpolate ${INTERPOLATE} -filter ${FILTER} -resize ${RESIZE} "${OUTPUT_BASENAME}_${TILE_INDEX}.png"
+
+        if [ "${IMAGE_CHANNELS}" == "rgba" ] || [ "${IMAGE_CHANNELS}" == "srgba" ]; then
+          magick convert "${INPUT_PATH}" -alpha extract -crop $((${TILE_WIDTH} + ${OVERDRAW}))x$((${TILE_HEIGHT} + ${OVERDRAW}))+${TILE_X1}+${TILE_Y1} +repage +adjoin -define png:color-type=2 -interpolate ${INTERPOLATE} -filter ${FILTER} -resize ${RESIZE} "${OUTPUT_BASENAME}_alpha_${TILE_INDEX}.png"
+        fi
+
+      done
+    done
+
+  else
+    echo "${FILENAME} is ${IMAGE_CHANNELS}, skipping"
+  fi
+
+}
+
 while read FILENAME; do
 
   DIRNAME=$(dirname "${FILENAME}")
-
-  BASENAME=$(basename "${FILENAME}")
-  BASENAME_NO_EXT="${BASENAME%.*}"
-
-  ESCAPED_DIR=$(printf '%q' "${DIRNAME}")
-  ESCAPED_FILE=$(printf '%q' "${FILENAME}")
-
+  BASENAME_NO_EXT=$(basename "${FILENAME%.*}")
   DIRNAME_HASH=$(echo ${DIRNAME} | md5sum | cut -d' ' -f1)
 
-  CATEGORY=$(echo ${BASENAME} | sed -ne "${CATEGORY_REGEXP}")
+  IMAGE_INFO=$(magick identify -format '%[width]:%[height]:%[channels]' "${FILENAME}")
+  IMAGE_WIDTH=$(echo ${IMAGE_INFO} | cut -d':' -f1)
+  IMAGE_HEIGHT=$(echo ${IMAGE_INFO} | cut -d':' -f2)
+  IMAGE_CHANNELS=$(echo ${IMAGE_INFO} | cut -d':' -f3)
 
-  if [ ! -f "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME_NO_EXT}_000.png" ]; then
+  wait_for_jobs
+  echo ${FILENAME}
+  create_tiles_task "${FILENAME}" "${OUTPUT_DIR}/${DIRNAME_HASH}_${BASENAME_NO_EXT}" ${IMAGE_WIDTH} ${IMAGE_HEIGHT} ${IMAGE_CHANNELS} &
 
-    IMAGE_INFO=$(magick identify -format '%[width] %[height] %[channels]' "${FILENAME}")
-    IMAGE_WIDTH=$(echo ${IMAGE_INFO} | cut -d' ' -f 1)
-    IMAGE_HEIGHT=$(echo ${IMAGE_INFO} | cut -d' ' -f 2)
-    IMAGE_CHANNELS=$(echo ${IMAGE_INFO} | cut -d' ' -f 3)
-
-    RELATIVE_DIR=$(realpath --relative-to "${INPUT_DIR}" "${DIRNAME}")
-
-    if [ "${IMAGE_WIDTH}" -ge "${MIN_TILE_WIDTH}" ] && [ "${IMAGE_HEIGHT}" -ge "${MIN_TILE_HEIGHT}" ]; then
-
-      VERTICAL_SUBDIVISIONS=$((${IMAGE_HEIGHT} / ${MAX_TILE_HEIGHT}))
-      if [ "${VERTICAL_SUBDIVISIONS}" -lt "1" ]; then
-        VERTICAL_SUBDIVISIONS=$((${IMAGE_HEIGHT} / ${MIN_TILE_HEIGHT}))
-      fi
-      HORIZONTAL_SUBDIVISIONS=$((${IMAGE_WIDTH} / ${MAX_TILE_WIDTH}))
-      if [ "${HORIZONTAL_SUBDIVISIONS}" -lt "1" ]; then
-        HORIZONTAL_SUBDIVISIONS=$((${IMAGE_WIDTH} / ${MIN_TILE_WIDTH}))
-      fi
-
-      if [ "$(magick convert "${FILENAME}" -alpha off -format "%[k]" info:)" -gt "1" ]; then
-        mkdir -p "${HR_OUTPUT_DIR}/${CATEGORY}/rgb"
-        mkdir -p "${LR_OUTPUT_DIR}/${CATEGORY}/rgb"
-        echo ${FILENAME}, rgb \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\) ${CATEGORY}
-        wait_for_jobs
-        magick convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${CATEGORY}/rgb/${DIRNAME_HASH}_${BASENAME_NO_EXT}_%03d.png" &
-        wait_for_jobs
-        magick convert "${FILENAME}" -alpha off -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${CATEGORY}/rgb/${DIRNAME_HASH}_${BASENAME_NO_EXT}_%03d.png" &
-      else
-        echo ${FILENAME}, rgb single color, skipped
-      fi
-      if [ "${IMAGE_CHANNELS}" == "rgba" ] || [ "${IMAGE_CHANNELS}" == "srgba" ]; then
-        if [ "$(magick convert "${FILENAME}" -alpha extract -format "%[k]" info:)" -gt "1" ]; then
-          mkdir -p "${HR_OUTPUT_DIR}/${CATEGORY}/alpha"
-          mkdir -p "${LR_OUTPUT_DIR}/${CATEGORY}/alpha"
-          echo ${FILENAME}, alpha \(${IMAGE_WIDTH}x${IMAGE_HEIGHT} divided by ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}\) ${CATEGORY}
-          wait_for_jobs
-          magick convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${HR_INTERPOLATE} -filter ${HR_FILTER} -resize ${HR_SCALE} "${HR_OUTPUT_DIR}/${CATEGORY}/alpha/${DIRNAME_HASH}_${BASENAME_NO_EXT}_alpha_%03d.png" &
-          wait_for_jobs
-          magick convert "${FILENAME}" -alpha extract -crop ${HORIZONTAL_SUBDIVISIONS}x${VERTICAL_SUBDIVISIONS}@ +repage +adjoin -define png:color-type=2 -interpolate ${LR_INTERPOLATE} -filter ${LR_FILTER} -resize ${LR_SCALE} "${LR_OUTPUT_DIR}/${CATEGORY}/alpha/${DIRNAME_HASH}_${BASENAME_NO_EXT}_alpha_%03d.png" &
-        else
-          echo ${FILENAME}, alpha single color, skipped
-        fi
-      fi
-
-    else
-      echo ${FILENAME} too small \(${IMAGE_WIDTH}x${IMAGE_HEIGHT}\), skipped
-    fi
-
-  else
-    echo ${FILENAME}, already processed, skipped
+  ROWS=$((${IMAGE_HEIGHT} / ${TILE_HEIGHT}))
+  if [ "${ROWS}" -le 0 ]; then
+    ROWS=1
   fi
-  
+  COLUMNS=$((${IMAGE_WIDTH} / ${TILE_WIDTH}))
+  if [ "${COLUMNS}" -le 0 ]; then
+    COLUMNS=1
+  fi
+
+  RELATIVE_DIR=$(realpath --relative-to "${INPUT_DIR}" "${DIRNAME}")
+
+  if [ "${IMAGE_CHANNELS}" == "rgba" ] || [ "${IMAGE_CHANNELS}" == "srgba" ]; then
+    echo "${DIRNAME_HASH}:${BASENAME_NO_EXT}:${RELATIVE_DIR}:rgba:${IMAGE_WIDTH}:${IMAGE_HEIGHT}:${ROWS}:${COLUMNS}" >> "${INDEX_FILE}"
+  elif [ "${IMAGE_CHANNELS}" == "rgb" ] || [ "${IMAGE_CHANNELS}" == "srgb" ]; then
+    echo "${DIRNAME_HASH}:${BASENAME_NO_EXT}:${RELATIVE_DIR}:rgb:${IMAGE_WIDTH}:${IMAGE_HEIGHT}:${ROWS}:${COLUMNS}" >> "${INDEX_FILE}"
+  fi
+
 done < <(find "${INPUT_DIR}" \( -iname "*.dds" -or -iname "*.png"  \))
 
 wait_for_jobs
